9047f86b990b130251d7c9112cd1ca3021799052
Sinan Rassam <sinan.rassam@outlook.com>
2020-05-30 12:34:06 +1200

    Added a new register activity

---
+++ app/app/src/main/java/com/example/ticketer/RegisterAcitivity.java
@@ -0,0 +1,14 @@
+package com.example.ticketer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
