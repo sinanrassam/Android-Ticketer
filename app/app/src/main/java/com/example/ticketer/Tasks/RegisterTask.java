aa88c754bf59329b71c63ab2259c0b6d942f1160
Sinan Rassam <sinan.rassam@outlook.com>
2020-05-31 15:21:41 +1200

    Added a Register Task

---
+++ app/app/src/main/java/com/example/ticketer/Tasks/RegisterTask.java
@@ -0,0 +1,25 @@
+package com.example.ticketer.Tasks;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Xml;
+
+public class RegisterTask extends AsyncTask<String, Void, Xml> {
+    @SuppressLint("StaticFieldLeak")
+    private Context mContext;
+
+    public RegisterTask(Context context) {
+        this.mContext = context;
+    }
+
+    @Override
+    protected Xml doInBackground(String... parameters) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Xml data) {
+
+    }
+}
