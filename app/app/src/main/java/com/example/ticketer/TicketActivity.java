95f403e3eb5fdd39df323ce037ea45916e99ba39
temaari <53286793+temaari@users.noreply.github.com>
2020-06-02 14:51:51 +1200

    this is adding the TicketTask

--- app/app/src/main/java/com/example/ticketer/TicketActivity.java
+++ app/app/src/main/java/com/example/ticketer/TicketActivity.java
@@ -1,13 +1,19 @@
 package com.example.ticketer;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.ticketer.Tasks.TicketTask;
+
 
 public class TicketActivity extends AppCompatActivity {
     private EditText mTitleView;
@@ -23,6 +29,47 @@ public class TicketActivity extends AppCompatActivity {
 
         mTitleView = findViewById(R.id.title);
         mDescriptionView = findViewById(R.id.desciption);
+
+        Button mSubmitBtn = findViewById(R.id.submitButton);
+
+        mSubmitBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptSubmit();
+            }
+        });
+    }
+
+    private void attemptSubmit() {
+        mTitleView.setError(null);
+        mDescriptionView.setError(null);
+        // mUsernameView.setError(null);
+
+        String title = mTitleView.getText().toString();
+        String desciption = mDescriptionView.getText().toString();
+        // String username = mUsernameView.getText().toString().toLowerCase();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // validate info
+        if (TextUtils.isEmpty(title)) {
+            mTitleView.setError(getString(R.string.error_field_required));
+            focusView = mTitleView;
+            cancel = true;
+        }
+        if (TextUtils.isEmpty(desciption)) {
+            mDescriptionView.setError(getString(R.string.error_field_required));
+            focusView = mDescriptionView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            TicketTask ticketTask = new TicketTask(this);
+            ticketTask.execute(title, desciption, "tickettester");
+        }
     }
 
 }
