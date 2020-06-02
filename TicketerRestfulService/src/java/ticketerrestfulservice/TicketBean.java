31827ef336542ca2e290b1339bdcae32fbea0c32
temaari <53286793+temaari@users.noreply.github.com>
2020-06-03 11:14:26 +1200

    creating ticketResource

--- TicketerRestfulService/src/java/ticketerrestfulservice/TicketBean.java
+++ TicketerRestfulService/src/java/ticketerrestfulservice/TicketBean.java
@@ -1,5 +1,6 @@
 package ticketerrestfulservice;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -11,7 +12,11 @@ public class TicketBean {
 
     @PostConstruct
     public void  initialiseTickets() {
-
+        tickets = new ArrayList<>();
+        addTicket("Internet issue", "there is an internet problem", "username1");
+        addTicket("Hardware issue", "My laptop seem's to be broken", "chris");
+        addTicket("Cooling Issue", "is it hot in here?", "Bobby");
+        addTicket("User Issue", "It's not working", "Jimz");
     }
 
     public void addTicket(String title, String description, String username) {
